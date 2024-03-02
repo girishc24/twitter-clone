14312db90dcf8b33f72c354c5ce5b695da22a48e
girishpluto <Girishpluto@gmail.com>
2024-03-03 02:46:19 +0530

    Working on Profile page and Admin View

--- social/views.py
+++ social/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from  . models import Profile
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html',)
 
-def home(request):
-    return render(request, 'home.html',)
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    context = {"profiles": profiles}
+    return render(request, 'profile_list.html', context)
